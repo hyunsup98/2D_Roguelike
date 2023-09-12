22c8c3afd7a07c6af660fe988e283032da49446d
hyunsup98 <134699838+hyunsup98@users.noreply.github.com>
2023-09-13 05:26:01 +0900

    .프로젝트 시작

--- Assets/0.Scripts/MapManager.cs
+++ Assets/0.Scripts/MapManager.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class MapManager : MonoBehaviour
 {
+    //<수정필요>보스맵, 보스맵은 총 3개이므로 추후 리스트로 변경될 예정
+    [SerializeField] Map bossMap;
     //맵 프리팹을 넣을 배열
     [SerializeField] List<Map> mapList = new List<Map>();
     //맵을 담을 위치 프리팹
     [SerializeField] TransMap transMap;
 
+    //조건 체크할 다음 맵을 넣는 리스트
+    List<Map> nextMap = new List<Map>();
+
     //맵 프리팹이 들어갈 배열
     public TransMap[,] maps;
+
     //2차원 배열의 [x, y]값
     public int x, y;
+
+    int a = -1;
 
     private void Awake()
     {
@@ -29,8 +37,12 @@ public class MapManager : MonoBehaviour
                 count++;
             }
         }
-
         SetStartMap();
+
+        foreach(var item in nextMap)
+        {
+            Debug.Log(item.name);
+        }
     }
 
 
@@ -52,124 +64,255 @@ public class MapManager : MonoBehaviour
         startMap.transform.SetParent(startPos.transform);
         startMap.transform.localPosition = Vector3.zero;
 
-        //맵의 2차원 배열 주소를 저장
-
-
-        while (true)
-        {
-            if (maps[x, y].map.isDown)
-            {
-                if (maps[x, y - 1].map.isUp)
-                {
-                    y--;
-                }
-            }
-        }
+        CheckCanMove(startMap, randRow, randCol);
     }
 
     //맵에서 이동할 수 있는 방향을 체크
     void CheckCanMove(Map map, int row, int col)
     {
-        //mapList의 남은 맵이 없는 경우 중단
-        if (mapList.Count == 0)
-            return;
+        a++;
 
+        //canMovePortalList에 조건이 맞는 포탈을 다 후보로 넣어둠
         List<GameObject> canMovePortalList = new List<GameObject>();
+        List<Map> spareMap = new List<Map>();
 
+        //조건1. 특정 방향에 포탈이 있는지
+        //조건2. 그 방향의 끝에 있는게 아닌지
+        //조건3. 서로 방향이 맞물리는 맵이 남아있는지 (up - down / left - right)
+        //위 조건들을 체크만 하고 해당되면 바로 canMovePortalList에 추가해준 뒤 실질적인 동작은 뒤에서 한다
+        //isUp
         if (map.isUp && row != 0)
         {
-            List<Map> isDownMap = new List<Map>();
-            foreach (var item in mapList)
+            if(maps[row - 1, col].map == null)
             {
-                if (item.isDown)
-                    isDownMap.Add(item);
-            }
+                List<Map> mm = new List<Map>();
+                foreach (var item in mapList)
+                {
+                    if (item.isDown)
+                        mm.Add(item);
+                }
+                if (mm.Count > 0)
+                {
+                    int rand = Random.Range(0, mm.Count);
+                    spareMap.Add(mm[rand]);
+                    mapList.Remove(mm[rand]);
 
-            if(isDownMap.Count != 0)
+                    canMovePortalList.Add(map.upPortal);
+                }
+                else
+                    map.isUp = false;
+            }
+            else
             {
-                canMovePortalList.Add(map.upPortal);
+                if (maps[row - 1, col].map.isDown)
+                {
+                    map.isUp = true;
+                    map.upPortal.gameObject.SetActive(true);
+                }
+                else
+                    map.isUp = false;
             }
         }
+        else
+            map.isUp = false;
 
+        //isDown
         if (map.isDown && row != maps.GetLength(0) - 1)
         {
-            canMovePortalList.Add(map.downPortal);
-        }
+            if(maps[row + 1, col].map == null)
+            {
+                List<Map> mm = new List<Map>();
+                foreach (var item in mapList)
+                {
+                    if (item.isUp)
+                        mm.Add(item);
+                }
+                if (mm.Count > 0)
+                {
+                    int rand = Random.Range(0, mm.Count);
+                    spareMap.Add(mm[rand]);
+                    mapList.Remove(mm[rand]);
 
+                    canMovePortalList.Add(map.downPortal);
+                }
+                else
+                    map.isDown = false;
+            }
+            else
+            {
+                if(maps[row + 1, col].map.isUp)
+                {
+                    map.isDown = true;
+                    map.downPortal.gameObject.SetActive(true);
+                }
+                else
+                    map.isDown = false;
+            }
+            
+        }
+        else
+            map.isDown = false;
+
+        //isLeft
         if (map.isLeft && col != 0)
         {
-            canMovePortalList.Add(map.leftPortal);
-        }
-
-        if (map.isRight && col != maps.GetLength(1) - 1)
-        {
-            canMovePortalList.Add(map.rightPortal);
-        }
-
-        int randPortal = Random.Range(1, canMovePortalList.Count);
-
-
-
-        //생성 가능한 맵 리스트가 없을 때 중단
-        if (mapList.Count == 0)
-            return;
-
-        List<TransMap> nextMapList = new List<TransMap>();
-        for (int x = -1; x < 1; x += 2)
-        {
-            for (int y = -1; y < 1; y += 2)
+            if(maps[row, col - 1].map == null)
             {
+                List<Map> mm = new List<Map>();
+                foreach (var item in mapList)
+                {
+                    if (item.isRight)
+                        mm.Add(item);
+                }
+                if (mm.Count > 0)
+                {
+                    int rand = Random.Range(0, mm.Count);
+                    spareMap.Add(mm[rand]);
+                    mapList.Remove(mm[rand]);
 
+                    canMovePortalList.Add(map.leftPortal);
+                }
+                else
+                    map.isLeft = false;
+            }
+            else
+            {
+                if(maps[row, col - 1].map.isRight)
+                {
+                    map.isLeft = true;
+                    map.leftPortal.gameObject.SetActive(true);
+                }
+                else
+                    map.isLeft = false;
             }
         }
+        else
+            map.isLeft = false;
 
-
-        /*
-        //이동 가능 방향 중 랜덤으로 포탈 만들기
-        int randMaxCreateCount = Random.Range(1, canMovePortalList.Count);
-
-        for (int i = 0; i < randMaxCreateCount; i++)
+        //isRight
+        if (map.isRight && col != maps.GetLength(1) - 1)
         {
-            //랜덤한 방향에 포탈 설치
-            int randPortal = Random.Range(0, canMovePortalList.Count);
-            PortalCreate(canMovePortalList[randPortal]);
-            canMovePortalList.RemoveAt(randPortal);
+            if(maps[row, col + 1].map == null)
+            {
+                List<Map> mm = new List<Map>();
+                foreach (var item in mapList)
+                {
+                    if (item.isLeft)
+                        mm.Add(item);
+                }
+                if (mm.Count > 0)
+                {
+                    int rand = Random.Range(0, mm.Count);
+                    spareMap.Add(mm[rand]);
+                    mapList.Remove(mm[rand]);
 
-            //mapList의 맵 중 랜덤으로 생성
-            int rand = Random.Range(0, mapList.Count);
-            Map nextMap = Instantiate(mapList[rand]);
-
-            //생선한 맵이 값 설정
-            nextMap.transform.SetParent(nextMapList[randPortal].transform);
-            nextMap.transform.localPosition = Vector3.zero;
-            nextMapList[randPortal].map = nextMap;
-            nextMapList.RemoveAt(randPortal);
+                    canMovePortalList.Add(map.rightPortal);
+                }
+                else
+                    map.isRight = false;
+            }
+            else
+            {
+                if(maps[row, col + 1].map.isLeft)
+                {
+                    map.isRight = true;
+                    map.rightPortal.gameObject.SetActive(true);
+                }
+                else
+                    map.isRight= false;
+            }
         }
-        */
+        else
+            map.isRight = false;
 
+        if (canMovePortalList.Count != 0)
+        {
+            //포탈을 몇 개 만들지 난수로 정함
+            int randPortal = Random.Range(1, canMovePortalList.Count + 1);
+
+            for (int i = 0; i < randPortal; i++)
+            {
+                //이어서 생성할 맵의 좌표
+                int x = row;
+                int y = col;
+
+                int rand = Random.Range(0, canMovePortalList.Count);
+
+                PortalCreate(canMovePortalList[rand]);
+
+                Map m = Instantiate(spareMap[rand]);
+
+                switch (canMovePortalList[rand].name)
+                {
+                    case "upPortal":
+                        x--;
+                        break;
+                    case "downPortal":
+                        x++;
+                        break;
+                    case "leftPortal":
+                        y--;
+                        break;
+                    case "rightPortal":
+                        y++;
+                        break;
+                }
+
+                m.transform.SetParent(maps[x, y].transform);
+                m.transform.localPosition = Vector3.zero;
+                maps[x, y].map = m;
+
+                nextMap.Add(m);
+
+                canMovePortalList.RemoveAt(rand);
+                spareMap.RemoveAt(rand);
+            }
+
+            OffBool(map, canMovePortalList);
+            foreach(var item in spareMap)
+            {
+                mapList.Add(item);
+            }
+            spareMap.Clear();
+        }
+
+        while(nextMap.Count > a)
+        {
+            CheckCanMove(nextMap[a], (int)nextMap[a].transform.parent.position.y * -1, (int)nextMap[a].transform.parent.position.x);
+        }
     }
-    /* 줄이려고 시도한 것
-    bool NextMapCheck()
+
+    //포탈 생성 조건에 만족해 리스트에 넣었지만 쓰이지 않은 방향을 false로 만듬
+    void OffBool(Map map, List<GameObject> list)
     {
-        List<Map> isWhereMap = new List<Map>();
-        foreach (var item in mapList)
+        //쓰이지 않은 포탈 방향을 false로 바꿈
+        foreach (var item in list)
         {
-            if ()
-                isWhereMap.Add(item);
+            switch (item.name)
+            {
+                case "upPortal":
+                    map.isUp = false;
+                    break;
+                case "downPortal":
+                    map.isDown = false;
+                    break;
+                case "leftPortal":
+                    map.isLeft = false;
+                    break;
+                case "rightPortal":
+                    map.isRight = false;
+                    break;
+            }
         }
-
-        if (isWhereMap.Count != 0)
-        {
-            return true;
-        }
-
-        return false;
     }
-    */
 
-    //맵의 특정 방향 포탈 열어주기
+    //특정 방향 포탈 열어주기
     void PortalCreate(GameObject portal)
     {
         portal.gameObject.SetActive(true);
     }
 }
+
+
+
+
